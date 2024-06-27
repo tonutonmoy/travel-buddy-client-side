49c69ee0a3b050e7ed1833d258d5f052f6725341
tonutonmoy <www.tonutonmoy12@gmail.com>
2024-06-27 17:44:21 +0600

    add menu

--- src/component/SideBar/SideBar.tsx
+++ src/component/SideBar/SideBar.tsx
@@ -28,14 +28,16 @@ const SideBar = ({ children }: { children: React.ReactNode }) => {
           }  `}
         >
           {children}{" "}
-          <label
-            htmlFor="my-drawer-2"
-            className={`align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-5  rounded-full bg-blue-500 hover:bg-blue-400 duration-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none lg:hidden  fixed ${
-              pathname === "/trip" ? "top-[10%] " : "top-3 "
-            }left-3`}
-          >
-            Menu
-          </label>
+          {!isOpen && (
+            <label
+              htmlFor="my-drawer-2"
+              className={`align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-5  rounded-full bg-blue-500 hover:bg-blue-400 duration-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none lg:hidden  fixed ${
+                pathname === "/trip" ? "top-[10%] " : "top-3 "
+              }left-3`}
+            >
+              Menu
+            </label>
+          )}
         </div>
         <div className="drawer-side ">
           <label
